61bb0ce94541421894e6af05b637ba7cae3d3e5e
Moiz <moizuddinr189@gmail.com>
2023-08-19 22:22:28 +0530

    fragments&mapFor_looping

--- 03-Passing_Data_Via_props/my-app/src/App.js
+++ 03-Passing_Data_Via_props/my-app/src/App.js
@@ -28,7 +28,7 @@ function App(){
   return(
     <div>
       <h2>Let's get started</h2>
-      <ExpenseItem 
+      {/* <ExpenseItem 
         title={expenses[0].title} 
         amount={expenses[0].amount} 
         date={expenses[0].date} 
@@ -52,7 +52,16 @@ function App(){
         date = {expenses[3].date}
         location={expenses[3].location}
       
-      ></ExpenseItem>
+      ></ExpenseItem> */}
+        
+{expenses.map((item) => (
+  <>
+    <p>{item.title} {item.amount}</p>
+    <p>{item.price} {item.location}</p>
+  </>
+))}
+
+      
 
     </div>
   );
