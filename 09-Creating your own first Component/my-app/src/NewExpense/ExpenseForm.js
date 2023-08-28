ccd8735ea1c8e729461496b7975d911c2d858d96
Moiz <moizuddinr189@gmail.com>
2023-08-29 00:40:56 +0530

    create-UI-DynamicFormFiller

--- 09-Creating your own first Component/my-app/src/NewExpense/ExpenseForm.js
+++ 09-Creating your own first Component/my-app/src/NewExpense/ExpenseForm.js
@@ -2,22 +2,38 @@ import React from 'react';
 import './ExpenseForm.css';
 
 function ExpenseForm(){
+   const titleChangehandler = (event) =>{
+      console.log(event.target.value);
+  }
+
+  const amountChangehandler = (event) =>{
+      console.log(event.target.value);
+
+  }
+
+  const dateChangeHandler = (event) =>{
+   console.log(event.target.value);
+
+  }
+ 
    return(
+      
+
       <form>
          <div className='new-expense__controls' >
            <div className='new-expense__control'>
             <label>TItle</label>
-            <input type='text' />
+            <input type='text' onChange={titleChangehandler}/>
            </div>
 
            <div className='new-expense__control'>
             <label>Amount</label>
-            <input type='number' min="0.01" step="0.01"/>
+            <input type='number' min="0.01" step="0.01" onChange={amountChangehandler}/>
            </div>
 
            <div className='new-expense__control'>
             <label>Date</label>
-            <input type='date' min="2019-01-01" max="2022-12-31"/>
+            <input type='date' min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
            </div>
          </div>
       <div className='new-expense__actions'>
