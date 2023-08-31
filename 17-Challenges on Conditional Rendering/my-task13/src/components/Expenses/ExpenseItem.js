ce351943db0af13785f8dbdab44766398c133af2
Moiz <moizuddinr189@gmail.com>
2023-08-31 23:08:36 +0530

    react challenge 2

---
+++ 17-Challenges on Conditional Rendering/my-task13/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
+
+const ExpenseItem = (props) => {
+  const[title, setTitle] = useState(props.title);
+  const[price,setPrice]  = useState(props.amount);
+  
+  const clickPriceHandler = () => {
+     setPrice("100")
+  }
+  const clickHandlerTitle = ()=>{
+    setTitle("Sharpener")
+    console.log(title);
+  }
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{title}</h2> 
+        <h3>{props.location}</h3>
+        <div className='expense-item__price'>${price}</div>
+        <button onClick={clickPriceHandler}>Change Expenses</button>
+        <button onClick={clickHandlerTitle}>Edit Title</button>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
