acfa14ec4538add5d0d11b5a9734b0915205a832
Moiz <moizuddinr189@gmail.com>
2023-09-03 01:47:42 +0530

    added Chart

---
+++ 18-ChartApp/my-tsk14/src/components/Expenses/Expenses.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import "./Expenses.css";
+import ExpenseList from "./ExpenseList";
+import ExpensesChart from "./ExpensesChart";
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  // let expensesContent = <p> "Only single Expense here. Please add more..."</p>;
+
+  // if (filteredExpenses.length === 1) {
+  //   expensesContent = filteredExpenses.map((expense) => (
+  //     <ExpenseItem
+  //       key={expense.id}
+  //       title={expense.title}
+  //       amount={expense.amount}
+  //       date={expense.date}
+  //       location={expense.location}
+  //     />
+  //   ));
+  // }
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesChart expenses = {filteredExpenses}/>
+        <ExpenseList items={filteredExpenses} />
+        {/* {filteredExpenses &&
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+              location={expense.location}
+            />
+          ))} */}
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
